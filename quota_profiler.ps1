73f25ec4543f742349e13038112529f3b0e5ea42
neotus75 <47954319+neotus75@users.noreply.github.com>
2022-04-28 17:16:32 +0800

    Update quota_profiler.ps1

--- quota_profiler.ps1
+++ quota_profiler.ps1
@@ -1,12 +1,16 @@
-﻿# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
 # ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
 # PARTICULAR PURPOSE.
 
-Clear-AzContext -Force
-Connect-AzAccount
+$context = Get-azContext
 
-
+if (!$context)
+{
+    Write-Output "There is no existing session.  Logging into Azure."
+    Clear-AzContext -Force
+    Connect-AzAccount
+}
 
 # retrives region list across the resources, and pull all the subscriptions in the tenant.
 $locations = Get-AzResource | ForEach-Object {$_.Location} | Sort-Object |  Get-Unique
@@ -77,42 +81,3 @@ foreach($subscription in $subscriptions)
 "@
         }
     }
- 
-    # Get Storage Quota
-
-    $storageQuota = Get-AzStorageUsage -Location $location -ErrorAction SilentlyContinue
-    $usage = 0
-
-    if ($storageQuota.Limit -gt 0) 
-    { 
-        $usage = $storageQuota.CurrentValue / $storageQuota.Limit 
-    }
-
-    $json += @"
-    { 
-        "Name":"$($storageQuota.LocalizedName)", 
-        "Location":"$location", 
-        "Category":"Storage", 
-        "CurrentValue":$($storageQuota.CurrentValue), 
-        "Limit":$($storageQuota.Limit),
-        "Usage":$usage 
-    }
-"@
-    
-    # Wrap in an array
-    $json = "[$json]"
-    $jsonObjects = $json | ConvertFrom-Json
-    $json = ''
-
-    $filename = $($currentAzContext.Subscription.Id)+".csv"
-
-    if (Test-Path -Path "C:\Temp") 
-    {
-        $jsonObjects | Export-Csv -Path $("C:\Temp\$filename") -NoTypeInformation
-    }
-    else
-    {
-        New-Item -Path "C:\Temp" -ItemType Directory
-        $jsonObjects | Export-Csv -Path $("C:\Temp\$filename") -NoTypeInformation
-    }
-}
