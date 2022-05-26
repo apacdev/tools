c4bd20c788e0868cc4f7b8752a4eb11632094fa6
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2022-05-26 13:19:33 +0800

    re-scripted to make it more readable and efficent.
    1. ouput screen got neater.
    2. use of New-Object and Array to get rid of chuncky json.

--- quota_profiler.ps1
+++ quota_profiler.ps1
@@ -6,8 +6,7 @@
 
 Clear-AzContext -Force
 Clear-Host
-$azaccount = Connect-AzAccount # -UseDeviceAuthentication # <= Uncomment this to use Device Authentication for MFA.
-$aztenant = Get-AzTenant
+Connect-AzAccount | Out-Null # -UseDeviceAuthentication # <= Uncomment this to use Device Authentication for MFA.
 
 # path to the outfile (csv) - if you are to use "relative location (e.g. c:\users\{your folder}\)"
 $datapath = "./QuotaUtil"
@@ -15,14 +14,13 @@ $datapath = "./QuotaUtil"
 # path to the outfile (csv) - if you are to use "absolute location"
 #$datapath = "c:/temp/QuotaUtil"
 
-if (!(Test-Path -Path $datapath/temp))
-{ 
+# see if the data path exists and create one if not.
+if (!(Test-Path -Path $datapath/temp)) { 
     New-Item $datapath/temp -ItemType Directory
 }
 
-# delete merged csv file to ensure no date is appended to old ones.
-if (Test-Path -Path $datapath\all_subscriptions.csv -PathType Leaf)
-{
+# delete merged csv file to ensure no data are appended to old ones.
+if (Test-Path -Path $datapath\all_subscriptions.csv -PathType Leaf) {
     Remove-Item -Path $datapath\all_subscriptions.csv -Force
 }
 
@@ -30,136 +28,106 @@ if (Test-Path -Path $datapath\all_subscriptions.csv -PathType Leaf)
 $locations = Get-AzResource | ForEach-Object {$_.Location} | Sort-Object |  Get-Unique
 $subscriptions = Get-AzSubscription
 $datetime = [System.DateTime]::UtcNow
-$json = ''
+$array = @()
 
 Write-Output "`n===== There are $($subscriptions.Count) subscription(s) ======`n"
 
 # loops through subscription list
-foreach($subscription in $subscriptions)
-{
+foreach($subscription in $subscriptions) {
+
     Write-Output "Currently fetching resource data from $subscription"
     
     # set the context from the current subscription
-    $subAzContext = Set-AzContext -Subscription $subscription
+    Set-AzContext -Subscription $subscription | Out-Null
     $currentAzContext = Get-AzContext
 
     # Get VM Quota and Utilization
-    foreach ($location in $locations)
-    {
+    foreach ($location in $locations) {
         # Get a list of Compute resources under the current subscription context
         $vmQuotas = Get-AzVMUsage -Location $location -ErrorAction SilentlyContinue
 
         # Get usage data of each Compute resources 
-        foreach($vmQuota in $vmQuotas)
-        {
-            $usage = 0
-            if ($vmQuota.Limit -gt 0)
-            {
-                $usage = $vmQuota.CurrentValue / $vmQuota.Limit
-            }
-            $json += @"
-            { 
-                "Date Time (UTC)":"$datetime",
-                "SubscriptionName":"$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))",
-                "Name":"$($vmQuota.Name.LocalizedValue)", 
-                "Category":"Compute", 
-                "Location":"$location", 
-                "CurrentValue":$($vmQuota.CurrentValue), 
-                "Limit":$($vmQuota.Limit),
-                "Usage":"$(([math]::Round($usage, 2) * 100).ToString())%"
-            },
-"@
+        foreach($vmQuota in $vmQuotas) {
+
+            $usage = ($vmQuota.Limit -gt 0) ? $($vmQuota.CurrentValue / $vmQuota.Limit) : 0
+            $object = New-Object -TypeName PSCustomObject
+            $object | Add-Member -Name 'datetime_in_utc' -MemberType NoteProperty -Value $datetime
+            $object | Add-Member -Name 'subscription_name' -MemberType NoteProperty -Value "$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))"
+            $object | Add-Member -Name 'resource_name' -MemberType NoteProperty -Value "$($vmQuota.Name.LocalizedValue)"
+            $object | Add-Member -Name 'location' -MemberType NoteProperty -Value $location
+            $object | Add-Member -Name 'current_value' -MemberType NoteProperty -Value $vmQuota.CurrentValue
+            $object | Add-Member -Name 'limit' -MemberType NoteProperty -Value $vmQuota.Limit
+            $object | Add-Member -Name 'usage' -MemberType NoteProperty -Value "$(([math]::Round($usage, 2) * 100).ToString())%"
+            $array += $object
         }
     }
 
     # Get Network Quota and its utilization
-    foreach ($location in $locations)
-    {
+    foreach ($location in $locations) {
+
         $networkQuotas = Get-AzNetworkUsage -Location $location -ErrorAction SilentlyContinue
-        foreach ($networkQuota in $networkQuotas)
-        {
-            $usage = 0
-
-            if ($networkQuota.limit -gt 0)
-            {
-                $usage = $networkQuota.currentValue / $networkQuota.limit
-            }
-
-            $json += @"
-            { 
-                "Date Time (UTC)":"$datetime",
-                "SubscriptionName":"$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))",
-                "Name":"$($networkQuota.name.localizedValue)", 
-                "Category":"Network",
-                "Location":"$location", 
-                "CurrentValue":$($networkQuota.currentValue), 
-                "Limit":$($networkQuota.limit),
-                "Usage":"$(([math]::Round($usage, 2) * 100).ToString())%"
-            },
-"@
+        
+        foreach ($networkQuota in $networkQuotas) {
+            $usage = ($networkQuota.Limit -gt 0) ? $($networkQuota.CurrentValue / $networkQuota.Limit) : 0
+            $object = New-Object -TypeName PSCustomObject
+            $object | Add-Member -Name 'datetime_in_utc' -MemberType NoteProperty -Value $datetime
+            $object | Add-Member -Name 'subscription_name' -MemberType NoteProperty -Value "$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))"
+            $object | Add-Member -Name 'resource_name' -MemberType NoteProperty -Value "$($networkQuota.Name.LocalizedValue)"
+            $object | Add-Member -Name 'location' -MemberType NoteProperty -Value $location
+            $object | Add-Member -Name 'current_value' -MemberType NoteProperty -Value $networkQuota.CurrentValue
+            $object | Add-Member -Name 'limit' -MemberType NoteProperty -Value $networkQuota.Limit
+            $object | Add-Member -Name 'usage' -MemberType NoteProperty -Value "$(([math]::Round($usage, 2) * 100).ToString())%"
+            $array += $object
         }
     }
     
     # Get Storage Quota and its utilization
     $storageQuota = Get-AzStorageUsage -Location $location -ErrorAction SilentlyContinue
-    $usage = 0
-    
-    if ($storageQuota.Limit -gt 0)
-    {
-        $usage = $storageQuota.CurrentValue / $storageQuota.Limit
-    }
-    
-    $json += @"
-    { 
-        "Date Time (UTC)":"$datetime",
-        "SubscriptionName":"$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))",
-        "Name":"$($storageQuota.LocalizedName)", 
-        "Location":"$location", 
-        "Category":"Storage", 
-        "CurrentValue":$($storageQuota.CurrentValue), 
-        "Limit":$($storageQuota.Limit),
-        "Usage":"$(([math]::Round($usage, 2) * 100).ToString())%" 
-    }
-"@
-    
-    # Wrap in an array
-    $json = "[$json]"
-    $jsonObjects = $json | ConvertFrom-Json
-    $json = ''
+    $usage = ($storageQuota.Limit -gt 0) ? $($storageQuota.CurrentValue / $storageQuota.Limit) : 0
+    $object = New-Object -TypeName PSCustomObject
+    $object | Add-Member -Name 'datetime_in_utc' -MemberType NoteProperty -Value $datetime
+    $object | Add-Member -Name 'subscription_name' -MemberType NoteProperty -Value "$($currentAzContext.Subscription.Name) ($($CurrentAzContext.Subscription.Id))"
+    $object | Add-Member -Name 'resource_name' -MemberType NoteProperty -Value "$($storageQuota.Name.LocalizedValue)"
+    $object | Add-Member -Name 'location' -MemberType NoteProperty -Value $location
+    $object | Add-Member -Name 'current_value' -MemberType NoteProperty -Value $storageQuota.CurrentValue
+    $object | Add-Member -Name 'limit' -MemberType NoteProperty -Value $storageQuota.Limit
+    $object | Add-Member -Name 'usage' -MemberType NoteProperty -Value "$(([math]::Round($usage, 2) * 100).ToString())%"
+    $array += $object
+
+    # saves outputs into .csv file
     $filename = $($currentAzContext.Subscription.Id)+".csv"
-    $jsonObjects | Export-Csv -Path $("$datapath/temp/$filename") -NoTypeInformation
+    $array | Export-Csv -Path $("$datapath/temp/$filename") -NoTypeInformation
+    $array = @()
 }
 
 $compress = @{
     Path = "$datapath/temp/*.csv"
     CompressionLevel = "Fastest"
     DestinationPath = "$datapath/quotautil.zip"
-  }
+}
 
+# reads in all .csv file for further processing (zipping, removeing duplicated column headers, etc.)
 $csvContent = Get-Content "$datapath/temp/*.csv"
 
 #Just a monkey way to remove repeated column headers from each csv files... Anyone with better idea?
 $index = 0
 Write-Output "`n===== Finalizing the output files... =====" 
-foreach ($line in $csvContent)
-{
-    if ($index -lt 1)
-    {
-        $line | Add-Content "$datapath/all_subscriptions.csv"    
+foreach ($line in $csvContent) {
+
+    if ($index++ -lt 1) {
+        #leave the first column header alone
+        $line | Add-Content "$datapath/all_subscriptions.csv"
     }
-    else
-    {
-        if (($line -notlike "*Date Time (UTC)*"))
-        {
-            $line | Add-Content "$datapath/all_subscriptions.csv"
+    else { 
+        # remove duplicated column headers for the rest
+        if (($line -notlike "*Date Time (UTC)*")) { 
+            $line | Add-Content "$datapath/all_subscriptions.csv" 
         }
      }
-    $index += 1
  }
 
 # zip up the individual files and clean up the temp files.
 Compress-Archive @compress -Force
 Remove-Item -Path "$datapath/temp/*" -Include *.csv
-
 Write-Output "`n===== Profiling completed =====" 
 Get-ChildItem -Path $datapath
