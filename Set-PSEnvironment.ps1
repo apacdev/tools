cc0e835b7077fe901c4e4735dceedabb78ef6fac
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2022-05-29 22:40:11 +0800

    Update Set-PSEnvironment.ps1

--- Set-PSEnvironment.ps1
+++ Set-PSEnvironment.ps1
@@ -1,35 +1,36 @@
-function Set-PSEnvironment {
-    # if running on Windows OS
-    if ([System.Environment]::OSVersion.Platform -eq 'Win32NT') {
+function Set-PSEnvironment() {
+     if ([System.Environment]::OSVersion.Platform -eq 'Win32NT') {
+        if ($true -eq (Test-Path 'HKLM:\SOFTWARE\Microsoft\PowerShellCore')) {
+        
+            Write-Output 'The installation of Powershell 7 is found on your machine.'
 
-        # install powershell 7.2+
-        if ([int] ($PSVersionTable.PSVersion.Major.ToString() + $PSVersionTable.PSVersion.Minor.ToString()) -lt 72) {
+            pwsh -NoProfile -ExecutionPolicy ByPass -Command {
 
-            # fetchs installation script from powershell github. the installation GUI will pop up.
-            Write-Host "Trying to install Powershell 7.2+ is now..."
-            Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -EnablePSRemoting -AddExplorerContextMenu"
-        }
+                If ($null -eq (Get-InstalledModule -Name Az -ErrorAction SilentlyContinue)) {
+                
+                    Write-Output 'Az modules are not found.  Installing the modules now. This may take a while...'
+                    Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -AllowClobber -Force -SkipPublisherCheck -PassThru
+                
+                } else {
 
-        # install az modules if it does not exist.
-        if ($null -eq (Get-InstalledModule -Name Az -ErrorAction SilentlyContinue)) {
+                    Write-Output 'Az modules are found. You are good to go!'
+                }
 
-            Write-Host "Installing Az Modules. This will take a while.."
-            if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-                    Start-Process pwsh -Verb RunAs "-NoProfile -ExecutionPolicy ByPass -Command Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"
-            }
+                if (-not $null -eq (Get-InstalledModule -Name AzureRM -ErrorAction SilentlyContinue)) {
 
-            Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -AllowClobber -Force -SkipPublisherCheck
-        }
-
-        # remove AzureRm modules if exists..
-        if (-not $null -eq (Get-InstalledModule -Name "AzureRm" -ErrorAction SilentlyContinue)) {
-
-            Write-Host "Uninstalling AzureRm Modules. This will take a while.."
-            if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-                    Start-Process powershell -Verb RunAs "-NoProfile -ExecutionPolicy ByPass -Command Uninstall-AzureRM"
+                    Write-Output 'AzureRM is found, and it is about to be removed. You need to give an administrator access if prompted.'
+                    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+                            Write-Output "Uninstalling AzureRm Modules. This will take a while..."
+                            Uninstall-AzureRM
+                    }
                 }
             }
+        }
+        else {
+                Write-Output 'The installation of Powershell 7 is not found on your machine. This will be installed...'
+                Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -EnablePSRemoting -AddExplorerContextMenu"
+                Write-Output 'Powershell 7 is now installed on your machine.  Please close and re-run the script.'
+        }
     }
 }
-
 Set-PSEnvironment
