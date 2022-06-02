12b87bf8a228e0af29b6273fb2525fcfc21b2c98
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2022-06-02 12:57:28 +0800

    Update Set-PSEnvironment.ps1

--- Set-PSEnvironment.ps1
+++ Set-PSEnvironment.ps1
@@ -20,11 +20,11 @@ function Install-Powershell() {
               Write-Host 'Powershell 7 is found on your Windows system...' 
         } 
         else { 
-              Write-Host 'The installation of Powershell 7 is not found on your machine. This will be installed...'
+            Write-Host 'The installation of Powershell 7 is not found on your machine. This will be installed...'
             try {
-                Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -EnablePSRemoting -AddExplorerContextMenu"
-                Write-Host 'Please CLOSE and REOPEN the current CommandLine (or PowerShell) window, then run the script again if PowerShell 7.0 is successfully installed.'
-                break;
+                    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -EnablePSRemoting -AddExplorerContextMenu"
+                    Write-Host 'Please CLOSE and REOPEN the current CommandLine (or PowerShell) window, then run the script again if PowerShell 7.0 is successfully installed.'
+                    break;
             }
             catch {
               Write-Host 'An error occurred during pulling the data from the remote server.  Please try again later...'
