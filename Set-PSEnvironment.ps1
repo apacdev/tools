33c659996882e6ad372e1c18e67854be0005ed9b
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2022-06-04 12:51:52 +0800

    Update Set-PSEnvironment.ps1

--- Set-PSEnvironment.ps1
+++ Set-PSEnvironment.ps1
@@ -1,3 +1,4 @@
+   
 # THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
 # ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
@@ -8,49 +9,44 @@
 # a quick and dirty way to check and setup running environment.
 ##########################################################################################
 
-# install the latest powershell on your system
 function Install-PowerShell() 
 {
-    if (([System.Environment]::OSVersion.Platform) -match 'Win32NT')
+
+    if (Test-Path 'HKLM:\SOFTWARE\Microsoft\PowerShellCore')
+    { 
+        Write-Host 'Powershell 7+ is already installed on your Windows.' 
+    } 
+    else 
     {
-        if (Test-Path 'HKLM:\SOFTWARE\Microsoft\PowerShellCore')
-        { 
-            Write-Host 'Powershell 7+ is already installed on your Windows.' 
-        } 
-        else 
+        Write-Host 'Powershell 7+ is not found on your Windows. The installation will start...'    
+        try 
         {
-            Write-Host 'Powershell 7+ is not found on your Windows. The installation will start...'    
-            try 
-            {
-                # retrieves the latest powershell from the Microsoft repo.
-                Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -EnablePSRemoting -AddExplorerContextMenu"
-                Write-Host 'Please CLOSE and REOPEN the current CommandLine (or PowerShell) window, and run the script again once PowerShell 7+ is successfully installed.'
-                break;
-            }
-            catch 
-            {
-                # there was an error in calling REST. 
-                Write-Host 'An error occurred during pulling the data from the remote server.  Please try again later...'
-            }
-        }  
-    }
+            # retrieves the latest powershell from the Microsoft repo.
+            powershell -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
+        }
+        catch 
+        {
+            # there was an error in calling REST. 
+            Write-Host 'An error occurred during pulling the data from the remote server.  Please try again later...'
+        }
+    }  
 }
 
-# install az modules if it does not exist on your machines.
+# install az modules for powershell 7 if it does not exist on your machines.
 function Install-AzModules() 
 {
-     pwsh -NoProfile -ExecutionPolicy ByPass -Command {
-          # command block to see if az modules are not found for your PowerShell 7, it fetches and install them.
-          If ($null -eq (Get-InstalledModule -Name Az -ErrorAction SilentlyContinue))
-          {
-               Write-Host 'No Az Modules are found on your system.  They will be installed now. It may take a while...'
-               Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -AllowClobber -Force -SkipPublisherCheck -PassThru
-          } 
-          else 
-          { 
-            Write-Output 'Az Modules are found...' 
-          }
-     }
+  $command = {
+    If ($null -eq (Get-InstalledModule -Name Az -ErrorAction SilentlyContinue))
+    {
+      Write-Host 'No Az Modules are found on your system.  They will be installed now. It may take a while...'
+      Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -AllowClobber -Force -SkipPublisherCheck -PassThru
+    } 
+    else 
+    { 
+      Write-Output 'Az Modules are found...' 
+    }
+ }
+  pwsh -NoProfile -PassThru -ExecutionPolicy ByPass -Command $command
 }
 
 # removes legacy AzureRM components to avoid conflicts with Az Modules.
@@ -80,12 +76,24 @@ function Remove-AzureRM()
 }
 
 # aggregates all fuction calls.
-function Set-PSEnvironment() {
+function Set-PSEnvironment()
+{
     Install-PowerShell
     Install-AzModules
     Remove-AzureRM
 }
+if (([System.Environment]::OSVersion.Platform) -match 'Win32NT')
+{
+    Set-PSEnvironment
 
-Set-PSEnvironment
+    $command = {
+      pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
+    }
 
-Write-Host 'The setup of Prerequisites is now completed.  Please proceed with running the quota_profiler script as described in Usage section in README.'
+    Start-Process -Wait "cmd" -ArgumentList '/c', $command 
+    Write-Host 'The setup of Prerequisites is now completed.  Please proceed with running the Get-AzQuotaUtil.ps1 script as described in Usage section in README.'
+}
+else 
+{
+    Write-Host 'This Pre-requisite script is written for Windows only.  For other OS like MacOS or Linux, please refer to "For MacOS or Linux Users" section of the README'
+}
