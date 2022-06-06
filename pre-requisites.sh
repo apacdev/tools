03b6ca89e9f9b73bf74f9788f8d58b9fad9da63c
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2022-06-06 13:52:27 +0800

    Update pre-requisites.sh

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -6,43 +6,69 @@
 
 #!/bin/bash
 
-clear
+function cecho() {
+    local exp=$1;
+    local color=$2;
+    
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    
+    tput setaf $color;
+    echo $exp;
+    tput sgr0;
+}
+
 OS="$(uname)"
 
 if [ $OS == "Darwin" ]; then
     if [ -z "$(which pwsh)" ]; then 
-        echo '[INFO] No valid PowerShell is found on your system. It will be installed.'
+        cecho '[INFO] No valid PowerShell is found on your system. It will be installed.' blue
         if [ -z "$(which brew)" ]; then
-            echo '[INFO] Homebrew is not installed on your system.  It is required to install PowerShell.' 
-            echo '[INFO] Installing Homebrew now...'
+            cecho '[ NO ] Homebrew is not installed on your system.  It is required to install PowerShell.' red
+            cecho '[INFO] Installing Homebrew now...' blue
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            echo '[INFO] Installing PowerShell now...'
+            cecho '[INFO] Installing PowerShell now...' blue
             brew install --cask powershell
+            cecho '[INFO] Installing other pre-requisites on your system now...' blue
             pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
         else
-            echo '[ OK ] HomeBrew is installed on your system.'
-            echo '[INFO] Installing PowerShell now...'
+            cecho '[ OK ] HomeBrew is installed on your system.' green
+            cecho '[INFO] Installing PowerShell now...' blue
             brew install --cask powershell
+            cecho '[INFO] Installing other pre-requisites on your system now...' blue
             pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
         fi
     else
-        echo '[ OK ] Valid owerShell is found on your system.'
+        cecho '[ OK ] Valid owerShell is found on your system.' green
         pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
     fi
+    
 elif [ $OS == "Linux" ]; then
     if [ -z "$(which pwsh)" ]; then
-        echo '[ NO ] No valid PowerShell is found on your system.'
+        cecho '[ NO ] No valid PowerShell is found on your system.' red
         sudo apt-get update
         sudo apt-get install -y wget apt-transport-https software-properties-common
         wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
         sudo dpkg -i packages-microsoft-prod.deb
         sudo apt-get update
+        cecho '[INFO] Installing PowerShell on your system now...' blue
         sudo apt-get install -y powershell
+        cecho '[INFO] Installing other pre-requisites on your system now...' blue
         pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
     else 
-        echo '[ OK ] Valid Powershell is found on your system.'
+        cecho '[ OK ] Valid Powershell is found on your system.' green
         pwsh -NoProfile -ExecutionPolicy ByPass -Command "Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ms-apac-csu/tools/main/Set-PSEnvironment.ps1'))"
     fi
 else
-    echo '[ NO ] No known OS is found on your system.  This script only supports MacOS or Linux (Ubuntu).'
+    cecho '[ NO ] No known OS is found on your system.  This script only supports MacOS or Linux (Ubuntu).' red
 fi
